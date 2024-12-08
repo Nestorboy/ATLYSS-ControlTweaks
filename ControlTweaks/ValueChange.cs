d7df6c28c2d6b9d094bb0fa3d0df47a2e1eb9731
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-12-08 18:57:21 +0100

    Added a struct for keeping track of value changes

---
+++ ControlTweaks/ValueChange.cs
@@ -0,0 +1,19 @@
+﻿namespace Nessie.ATLYSS.ControlTweaks;
+
+public struct ValueChange<T>(T value = default)
+{
+    public bool Changed { get; private set; }
+
+    private T _value = value;
+
+    public void SetValue(T value)
+    {
+        Changed = !_value.Equals(value);
+        _value = value;
+    }
+
+    public static implicit operator T(ValueChange<T> value)
+    {
+        return value._value;
+    }
+}
