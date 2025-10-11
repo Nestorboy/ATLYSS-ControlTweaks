5cad7a65e48cc757b4503ae642f95bb7c38c4523
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2025-10-11 03:18:05 +0200

    Add jump buffer support for gamepads

--- ControlTweaks/Patches/JumpBufferPatches.cs
+++ ControlTweaks/Patches/JumpBufferPatches.cs
@@ -18,13 +18,13 @@ public static class JumpBufferPatches
         [HarmonyPrefix]
         private static void RecordInputPressed(PlayerMove __instance) // ReSharper restore InconsistentNaming
         {
-            _inputJumpDown = Input.GetKeyDown(InputControlManager.current._jump);
+            _inputJumpDown = Input.GetKeyDown(InputControlManager.current._jump) || (GamepadInput.GetButtonDown(GamepadButton.Button_South, InputControlManager.current._setGamepadInfo) && !__instance._player._inUI);
             _canJump = CanJumpInput(__instance);
             if (_inputJumpDown)
             {
                 _inputJump.UpdateState(true);
             }
-            else if (Input.GetKeyUp(InputControlManager.current._jump))
+            else if (Input.GetKeyUp(InputControlManager.current._jump) || (GamepadInput.GetButtonUp(GamepadButton.Button_South, InputControlManager.current._setGamepadInfo) && !__instance._player._inUI))
             {
                 _inputJump.UpdateState(false);
             }
@@ -139,11 +139,11 @@ public static class JumpBufferPatches
         [HarmonyPrefix]
         private static bool RecordInputPressed(PlayerClimbing __instance) // ReSharper restore InconsistentNaming
         {
-            if (Input.GetKeyDown(InputControlManager.current._jump))
+            if (Input.GetKeyDown(InputControlManager.current._jump) || GamepadInput.GetButtonDown(GamepadButton.Button_South, InputControlManager.current._setGamepadInfo))
             {
                 _inputJump.UpdateState(true);
             }
-            else if (Input.GetKeyUp(InputControlManager.current._jump))
+            else if (Input.GetKeyUp(InputControlManager.current._jump) || GamepadInput.GetButtonUp(GamepadButton.Button_South, InputControlManager.current._setGamepadInfo))
             {
                 _inputJump.UpdateState(false);
             }
